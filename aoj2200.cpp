8d4c2836f1fa0e51cf6089ea73702097e1522441
Jing Guan <jingg.cxy@foxmail.com>
2017-01-19 13:49:51 +0800

    最短路

--- aoj2200.cpp
+++ aoj2200.cpp
@@ -2,7 +2,7 @@
  * AOJ 2200: Mr. Rito Post Office
  * 题意：给出岛之间的若干水路和陆路，水路转为陆路时船就停在那，之后要陆路转水路还得从那开始。求按给出的可重复序列通过多个岛的最短时间。
  * 类型：最短路+DP
- * 算法：先用Floyd预处理出单独走水路s或陆路l的两两之间的最短路。d[i][j]表示已经达到序列第i个点，并将船停在了j的位置。人从a到b，船从c到d，则时间要加上l[a,c]+s[c,d]+l[d,b]。再用s[a,b]+l[b,a]的环路将船移动而人不动，使得可能出现的状态d值都合法。
+ * 算法：先用Floyd预处理出单独走水路s或陆路l的两两之间的最短路。d[i][j]表示到序列第i点时船停在j。状态转移：人从a到b，船从c到d，若船动加上l[a,c]+s[c,d]+l[d,b]，若船不动则只要加上l[a][b]。
  */
 
 #include <iostream>
@@ -79,14 +79,16 @@ int main() {
       for(int j = 1; j <= n; ++j) {
         if(l[j][b[i]] >= INF) continue;
         for(int k = 1; k <= n; ++k) {
-          if(d[i-1][k] < INF && l[b[i-1]][k] < INF && s[k][j] < INF) {
-            d[i][j] = min(d[i][j], d[i-1][k] + l[b[i-1]][k] + s[k][j] + l[j][b[i]]);
+          if(d[i-1][k] >= INF) continue;
+          if(j == k){
+            if(l[b[i-1]][b[i]] < INF) {
+              d[i][j] = min(d[i][j], d[i-1][k] + l[b[i-1]][b[i]]);
+            }
+          } else{
+            if(l[b[i-1]][k] < INF && s[k][j] < INF) {
+              d[i][j] = min(d[i][j], d[i-1][k] + l[b[i-1]][k] + s[k][j] + l[j][b[i]]);
+            }
           }
-        }
-      }
-      for(int j = 1; j <= n; ++j) {
-        if(d[i][b[i]] < INF && q[b[i]][j] < INF) {
-          d[i][j] = min(d[i][j], d[i][b[i]] + q[b[i]][j]);
         }
       }
     }
